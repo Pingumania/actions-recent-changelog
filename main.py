f38cd48a928b19faef93b9a947442fab2aee2da5
Pingumania <551155+Pingumania@users.noreply.github.com>
2026-07-26 12:28:05 +0200

    Make separator configurable

--- main.py
+++ main.py
@@ -2,27 +2,19 @@
 
 import sys
 import os.path
-import re
 
-def main(full_changelog, output):
+def main(full_changelog, output, separator='*****'):
 	if not os.path.isfile(full_changelog):
 		sys.exit("Input file doesn't exist")
 	with open(full_changelog, 'r') as f, open(output, 'w') as out:
-		matching = False
 		lines = f.readlines()
 		for line in lines:
-			if re.search(r'^.* \| .*$', line):
-				if matching:
-					print("Wrote changelog to", output)
-					return
-				matching = True
-			if matching:
-				if not line.isspace():
-					out.write(line)
-				if line is lines[-1]:
-					print("Wrote changelog to", output)
-					return
-	sys.exit("Couldn't write changelog")
+			if line.strip() == separator:
+				print("Wrote changelog to", output)
+				return
+			if not line.isspace():
+				out.write(line)
+	print("Wrote changelog to", output)
 
 if __name__ == '__main__':
-	main(sys.argv[1], sys.argv[2])
+	main(sys.argv[1], sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else '*****')
